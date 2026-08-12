0fb4665464a2d026ae66209f947fa3912614c514
Andres Torres <andres.torres@grafana.com>
2026-08-12 09:48:13 -0400

    feat(configprovider): add GetSections to the config provider interface

--- pkg/configprovider/configprovider.go
+++ pkg/configprovider/configprovider.go
@@ -3,12 +3,15 @@ package configprovider
 import (
 	"context"
 
+	"gopkg.in/ini.v1"
+
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/setting"
 )
 
 type ConfigProvider interface {
 	Get(context.Context) (*setting.Cfg, error)
+	GetSections(context.Context, ...string) (*ini.File, error)
 }
 
 type OSSConfigProvider struct {
@@ -19,6 +22,28 @@ type OSSConfigProvider struct {
 func (c *OSSConfigProvider) Get(_ context.Context) (*setting.Cfg, error) {
 	c.log.Debug("OSSConfigProvider Get")
 	return c.Cfg, nil
+}
+
+func (c *OSSConfigProvider) GetSections(_ context.Context, sections ...string) (*ini.File, error) {
+	c.log.Debug("OSSConfigProvider GetSections")
+
+	result := ini.Empty()
+	for _, section := range sections {
+		src, _ := c.Cfg.Raw.GetSection(section)
+		if src == nil {
+			continue
+		}
+		dst, err := result.NewSection(section)
+		if err != nil {
+			return nil, err
+		}
+		for _, key := range src.Keys() {
+			if _, err := dst.NewKey(key.Name(), key.Value()); err != nil {
+				return nil, err
+			}
+		}
+	}
+	return result, nil
 }
 
 func ProvideService(cfg *setting.Cfg) (ConfigProvider, error) {
